fd96079996eb976e06bc18635764c2aafb737cbd
Shajham miah <mdshahjhan478877@gmail.com>
2022-08-11 08:08:18 +0600

    first commit

---
+++ assignments.js
@@ -0,0 +1,104 @@
+//problem1- radianToDegree convert
+/*function radianToDegree(radian){
+    const degree =radian * 180/3.1416;
+   return degree;
+ 
+   }
+   var RadianToDegree=10;
+   
+   console.log(radianToDegree( RadianToDegree));
+   */
+//......//
+
+
+   //Problem:2  isJavaScriptFile 
+
+   /*function  isJavaScriptFile (file='index.js'){   
+        return true;                      
+          
+   }
+   console.log(isJavaScriptFile('index.js'))
+   */
+   
+   
+
+  //........//
+
+
+   //Problem 3: oilPrice
+
+   /*function oilPrice( diesel, petrol,octan){
+    const diselPrice = 114;
+    const petrolPrice =130;
+    const octanPrice=135;
+    const diselAndPrice= diselPrice * diesel;
+    const petrolAndPrice= petrolPrice * petrol;
+    const octanAndPrice= octanPrice * octan;
+     const totalPrice=  diselAndPrice + petrolAndPrice +  octanAndPrice;
+      return totalPrice;
+   }
+
+   console.log(oilPrice(1,1,1));
+   */
+  
+   
+
+
+
+
+//problem4-publicBusFare
+
+/*function publicBusFare(people){
+    const busPeople=50;
+    const microbus=11;
+    const totalPeople=65-50;
+    const busremaining=15;
+    const totalRemaining= microbus - busremaining;
+    const microbuspeople=4;
+    const publicbuc=4
+    const publicbusFare=250;
+    const totlafare= publicbuc * publicbusFare;
+    return totlafare;
+    
+   
+
+
+}
+
+console.log(publicBusFare(50));
+*/
+
+
+
+
+
+
+//Problem 5: isBestFriend
+/*const student=[
+    {name:'Tom', friend:'Rock'},
+    {name:'Rock',friend:'Tom'},
+    {name:'chris', friend:'Rock'},
+    {name:'Rock', friens:'jony'}
+]
+
+function isBestFriend( friend, name){
+    for(let i =0; i<friend.length; i++){
+        const element=friend[i];
+        if(element == friend[i]){
+            friend =element;
+           
+        }
+        
+        else if(element == name[i] ){
+            name=element;
+        }
+        //return false;
+
+       
+        
+        //console.log(element);
+    }
+  return true;
+}
+console.log(isBestFriend(student));
+*/
